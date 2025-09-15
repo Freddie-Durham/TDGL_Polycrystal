87684e00d8e73619d389ec8907090b0446b2ead8
Freddie-Durham <wzjr67@durham.ac.uk>
2025-09-15 09:24:21 +0100

    create all neper 2D .tess files

--- scripts/main.jl
+++ scripts/main.jl
@@ -2,12 +2,13 @@ using TDGL_Polycrystal
 
 function main()
     package_dir = abspath(joinpath(@__DIR__,".."))*"neper/2D_crystal"
-    id = 1
-    
-    for i in 10:500
-        filename = package_dir*"D$(i)AR100xyID$id"
-        if !isfile(filename*".tess")
-            run(`neper -T -n from_morpho -morpho "graingrowth($(i/1000))" -dim 2 -periodicity 1 -id $id -o $filename`)
+
+    for id in 1:2
+        for i in 10:500
+            filename = package_dir*"D$(i)AR100xyID$id"
+            if !isfile(filename*".tess")
+                run(`neper -T -n from_morpho -morpho "graingrowth($(i/1000))" -dim 2 -periodicity 1 -id $id -o $filename`)
+            end
         end
     end
 end
